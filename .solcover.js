1ee24a85b700ba6e61562bbc3ce9a63ab1c4752b
WhiteBuffaloUniversity <whitebuffalouniversity@whitebuffalo.fans>
2022-05-28 18:22:26 +0000

    love

--- .solcover.js
+++ .solcover.js
@@ -1,15 +1,13 @@
-module.exports = {
-    norpc: true,
-    testCommand: 'npm test',
-    compileCommand: 'npm run compile',
-    skipFiles: [
-        'mocks',
-    ],
-    providerOptions: {
-        default_balance_ether: '10000000000000000000000000',
-    },
-    mocha: {
-        fgrep: '[skip-on-coverage]',
-        invert: true,
-    },
-}
+export const norpc = true;
+export const testCommand = 'npm test';
+export const compileCommand = 'npm run compile';
+export const skipFiles = [
+    'mocks',
+];
+export const providerOptions = {
+    default_balance_ether: '10000000000000000000000000',
+};
+export const mocha = {
+    fgrep: '[skip-on-coverage]',
+    invert: true,
+};
